d7aa3d1353f822f3391eaef40a129dca6017dfda
Hao Luo <howlowck@gmail.com>
2023-12-29 12:08:27 -1000

    changed to 18

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -4,14 +4,13 @@ import React, { PropsWithChildren } from 'react'
 import { Provider } from 'react-redux'
 import AppStore, { setupStore } from './redux/store'
 import { type ReduxState } from './redux/reducer'
-import type { PreloadedState } from '@reduxjs/toolkit'
 
 // This type interface extends the default options for render from RTL, as well
 // as allows the user to specify other things such as initialState, store. For
 // future dependencies, such as wanting to test with react-router, you can extend
 // this interface to accept a path and route and use those in a <MemoryRouter />
 type ExtendedRenderOptions = {
-  preloadedState?: PreloadedState<ReduxState>
+  preloadedState?: ReduxState
   store?: typeof AppStore
 } & Omit<RenderOptions, 'queries'>
 
